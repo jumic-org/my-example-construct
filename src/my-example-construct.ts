7e3ce1878dbd804507dfdde2fe558c7c43b711dc
Julian Michel <mail@julianmichel.de>
2026-04-04 12:30:27 +0200

    feat: use bucket encryption to improve security

--- src/my-example-construct.ts
+++ src/my-example-construct.ts
@@ -1,5 +1,6 @@
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as kms from 'aws-cdk-lib/aws-kms';
 
 export interface MyConstructProps {
   readonly bucketName?: string;
@@ -9,9 +10,15 @@ export class MyExampleConstruct extends Construct {
   constructor(scope: Construct, id: string, props: MyConstructProps = {}) {
     super(scope, id);
 
+    const key = new kms.Key(this, 'MyKey', {
+      enableKeyRotation: true,
+    })
+
     new s3.Bucket(this, 'MyBucket', {
       bucketName: props.bucketName,
       enforceSSL: true,
+      encryption: s3.BucketEncryption.KMS,
+      encryptionKey: key,
     });
   }
 }
